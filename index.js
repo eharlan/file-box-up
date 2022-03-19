7eac2e1abafd042b7e09a0201f0c4b3bfc038d85
Erik Harlan <harlanes14@gmail.com>
2022-03-19 16:07:23 -0600

    Working upload with progress

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const multer = require('multer');
 const morgan = require('morgan');
 
 const UPLOAD_PATH = 'uploads/';
-const upload = multer({ dest: UPLOAD_PATH })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    var filename = file.originalname;
+    var fileExtension = filename.split(".")[1];
+    cb(null, filename+'_'+new Date().toISOString());
+  }
+});
+
+const upload = multer({ storage: storage })
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+app.options('*', cors());
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
@@ -21,9 +33,11 @@ app.get('/', (req, res) => {
   res.send('Printer API')
 });
 
-app.post(`/${UPLOAD_PATH.replace('/','')}`, upload.array('files'), (req, res) => {
+app.post(`/${UPLOAD_PATH.replace('/','')}`, upload.array('files', 100), (req, res) => {
     console.log('Got body:', req.body, 'files:', req.files);
-    res.sendStatus(200);
+    res.send(req.files);
+}, (error, req, res, next) => {
+	res.status(400).send({error: error.message})
 });
 
 app.listen(port, () => {
--- public/index.js
+++ public/index.js
@@ -1,9 +1,20 @@
 const input = document.getElementById('fileUpload')
+
+const bar = document.querySelector('.progress');
+const barProgress = document.querySelector('.progress-bar');
+const alertBox = document.querySelector('.alert-success');
+
+
 let selectedFiles = null;
 
 input.onchange = function(event) {
     selectedFiles = event.target.files;
     
+    if(alertBox.classList.contains('show')){
+     alertBox.classList.toggle('d-none');
+     alertBox.classList.toggle('show');
+    }
+
     for (const file of selectedFiles) {
         console.log(file.name, file.size)
     }
@@ -12,8 +23,45 @@ input.onchange = function(event) {
 
 function formSubmit(event){
     event.preventDefault();
-    axios.post('/uploads',{
-        files:selectedFiles
-    })
+    
+    
+	if(alertBox.classList.contains('show')){
+	  alertBox.classList.toggle('show');
+	  alertBox.classList.toggle('d-none');
+	}
 
+	bar.classList.toggle('d-none')
+
+    const bodyFormData = new FormData();
+	for(const file of selectedFiles){
+	bodyFormData.append('files', file); 	
+	}
+
+    axios({
+			method: 'post',
+			url: '/uploads',
+			data: bodyFormData,
+	    		onUploadProgress: p => {
+			  const value = Math.floor((p.loaded/p.total)*100)+'%';
+			  barProgress.style.width = value;
+			  barProgress.innerHTML = value;
+
+			},
+			headers: {"Content-Type": "multipart/form-data"}
+		})
+		.then(function(response){
+			console.log('success!: '+response);
+			alertBox.classList.toggle('show');
+			alertBox.classList.toggle('d-none');
+		})
+		.catch(function(error){
+			console.log('Upload error: '+error)
+		})
+		.finally(function(){
+		setTimeout(() => {
+		  bar.classList.toggle('d-none');
+		  barProgress.style.width = '0%';
+		  barProgress.innerHTML = '0%';
+		},2000)		
+		});
 }
